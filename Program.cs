fb47463ec32e56862692b56d5a81fefce2b53551
poonambudde <buddepoonam123@gmail.com>
2022-02-16 05:19:39 +0530

    [Poonam] ADDED: Basic Core Programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+        }
+    }
+}
