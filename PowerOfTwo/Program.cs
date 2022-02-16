7c149f5efa4bb337a7dc12f0a6ec8156ba774269
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-16 17:46:51 +0530

    [Poonam] ADDED: print table of power of 2

---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int power_2;
+            Console.WriteLine("Please enter value of n to print power of 2 table");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 0 && n <= 30)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    power_2 = Convert.ToInt32(Math.Pow(2, i));
+                    Console.WriteLine("2^" + i + " : " + power_2);
+                }
+            }
+            else
+                Console.WriteLine("Please Enter value of n from 0 to 30");
+        }
+    }
+}
