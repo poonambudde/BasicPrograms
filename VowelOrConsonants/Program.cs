d25a2c39b1ccf5b2d10270231bbd6ee032675b15
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-16 17:43:06 +0530

    [Poonam] ADDED: check which is vowel or consonants

---
+++ VowelOrConsonants/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrConsonants
+{
+    internal class Programs
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Enter a character to check wheather it is Vowel or Consonant : ");          
+            char character = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch (character)
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("Entered character is a Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Entered character is a Consonant");
+                    break;
+            }
+        }
+    }
+}
