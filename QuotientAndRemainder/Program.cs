9e1b718f785f6310dec250d7ad09622968535e41
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-02-16 17:39:55 +0530

    [Poonam] ADDED: compute quotient and remainder

---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientAndRemainder 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend, divisor, quotient, remainder;
+            Console.WriteLine("Enter Dividend and divisor");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            divisor = Convert.ToInt32(Console.ReadLine());
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is " + remainder);
+        }
+    }
+}
